85d66479671e7631d4da2042dc9c3238ba30fa06
homies886 <Niko876@hotmail.com>
2019-09-18 14:55:08 -0400

    Updated user controller and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :captures
     has_many :captured_pokemons, through: :captures, source: :pokemon
     has_many :pokemons
+
+    has_secure_password
+
+    validates :username, uniqueness: true, presence: true
 end
