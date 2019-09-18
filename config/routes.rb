28efef7ff3fa48785b6adb5dd088197718f67592
homies886 <Niko876@hotmail.com>
2019-09-18 14:34:07 -0400

    routes and index for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   get '/' => 'sessions#index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post 'signup' => 'users#create'
+
   resources :captures
   resources :pokemons
   resources :regions
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
