e546f698bf98358bb4562507a6b9b7e75621ac5e
homies886 <Niko876@hotmail.com>
2019-09-18 14:46:38 -0400

    Addes sign up page and fixed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :captures
-    has_many :captured_pokemons, through :captures, source :pokemon
+    has_many :captured_pokemons, through: :captures, source: :pokemon
     has_many :pokemons
 end
