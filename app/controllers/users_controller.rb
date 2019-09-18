2d6d1c618f2bf8621846a8d8e3dc5cd71b34308a
homies886 <Niko876@hotmail.com>
2019-09-18 15:09:09 -0400

    Edited user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,25 +4,29 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+    def show
+        if logged_in?
+            @user = User.find_by(id: params[:id])
         else
-            render :new
+            redirect_to root_path
         end
     end
 
-    def show
-        @user = User.find_by_id(params[:id])
-        redirect_to '/' if !@user
+    def create
+        @user = User.new(user_params)
+    
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to @user
+        else
+          render :new
+        end
     end
-
+    
     private
 
     def user_params
-        params.require(:user).permit(username, password)
+        params.require(:user).permit(:username, :password)
     end
 
 end
