6d2c1403bef55852ec0c9fcc8c7156202e8caa1e
homies886 <Niko876@hotmail.com>
2019-09-20 12:35:54 -0400

    Added login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
     def index
     end
 
+    def new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Sorry, username and/or password is incorrect. Please try again."
+            redirect_to login_path
+        end
+    end
+
     def destroy
         session.delete[:user_id]
         redirect_to '/'
