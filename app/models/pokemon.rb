d03869c87d9533a11fb7a8879faf4a3e09fde4a0
homies886 <Niko876@hotmail.com>
2019-12-11 13:48:05 -0500

    validation for pokemon

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -4,4 +4,15 @@ class Pokemon < ApplicationRecord
   has_many :captures
   has_many :users, through :captures
   accepts_nested_attributes_for :region
+
+  validates :name, presence: true
+  validate :not_a_duplicate
+
+  def not_a_duplicate
+    pokemon = Pokemon.find_by(name: name, region_id: region_id)
+    if !!pokemon && pokemon != self
+      errors.add(:name, 'has already been added from that region')
+    end
+  end
+
 end
