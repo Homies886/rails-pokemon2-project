d5cfeae3f3b6b8f6e5160390a1d909e4c5567e55
homies886 <Niko876@hotmail.com>
2019-12-15 16:55:28 -0500

    Adding email fields to params, forms, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, uniqueness: true, presence: true
+    validates :email, presence: true
 
     def self.create_by_google_omniauth(auth)
         self.find_or_create_by(username: auth[:info][:email]) do |u|
