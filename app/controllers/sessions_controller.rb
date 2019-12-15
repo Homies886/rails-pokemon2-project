5bfc7e4858f824d37d3655f68a45a87727f1bc87
homies886 <Niko876@hotmail.com>
2019-12-15 17:44:23 -0500

    syntax

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -33,7 +33,7 @@ class SessionsController < ApplicationController
     end
 
     def omniauth
-        @user = User.create_by_google_omniauth(auth)
+        @user = User.create_by_google(auth)
     
         session[:user_id] = @user.id
         redirect_to user_path(@user)
