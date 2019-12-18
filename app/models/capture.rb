d3c9230c68b0ca0586452c6aea6dbe0273f9befa
homies886 <Niko876@hotmail.com>
2019-12-18 14:03:16 -0500

    added date validation

--- app/models/capture.rb
+++ app/models/capture.rb
@@ -6,6 +6,16 @@ class Capture < ApplicationRecord
 
   validates :pokemon, uniqueness: { scope: :user, message: "has already been caught by you" }
 
+  validate :is_valid_catch_date
+
   scope :order_by_date_captured, -> {order(captured: :desc)}
+
+  private
+
+  def is_valid_catch_date
+    if Date.today < captured
+      errors.add(:captured, 'Sorry, date captured cannot be in the future.')
+    end
+  end
 
 end
